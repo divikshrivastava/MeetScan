36556eb77959ff898f37166cf68ebe3024024b9d
Wilfredo Concepcion <151902549+wlfrdoconc@users.noreply.github.com>
2024-10-23 17:52:08 -0700

    Update ScanArea.tsx
    Added tiktok link recognization

--- src/components/ScanArea.tsx
+++ src/components/ScanArea.tsx
@@ -6,6 +6,7 @@ import linkedinIcon from '../icons/linkedin.svg';
 import bitlyIcon from '../icons/bitly.svg';
 import instagramIcon from '../icons/instagram.svg';
 import websiteIcon from '../icons/website.svg';
+import tiktokIcon from '../icons/tiktok.svg';
 
 interface ScanAreaProps {
   onSave: (scan: any) => void;
@@ -18,8 +19,9 @@ const ScanArea: React.FC<ScanAreaProps> = ({ onSave }) => {
   const [notes, setNotes] = useState('');
   const [showScanner, setShowScanner] = useState(true); // Active scanner by default
   const [qrCodeImage, setQrCodeImage] = useState<string | null>(null);
-  const [linkType, setLinkType] = useState<'linkedin' | 'bitly' | 'instagram' | 'website' | 'unknown'>('unknown'); // Type of link detected
-
+  const [linkType, setLinkType] = useState<'linkedin' | 'bitly' | 'tiktok' | 'instagram' | 'website' | 'unknown'>('unknown'); // Type of link detected
+  
+  
   useEffect(() => {
     const savedQrCode = localStorage.getItem('qrCode');
     if (savedQrCode) {
@@ -48,6 +50,8 @@ const ScanArea: React.FC<ScanAreaProps> = ({ onSave }) => {
       setLinkType('bitly');
     } else if (/instagram\.com/.test(url)) {
       setLinkType('instagram');
+    } else if (/tiktok\.com/.test(url)) {
+      setLinkType('tiktok');
     } else if (/^(https?:\/\/)?([\w\d-]+\.)+[\w-]+/.test(url)) {
       setLinkType('website');
     } else {
@@ -78,6 +82,8 @@ const ScanArea: React.FC<ScanAreaProps> = ({ onSave }) => {
         return <img src={bitlyIcon} alt="Bitly" className="h-6 w-6" />;
       case 'instagram':
         return <img src={instagramIcon} alt="Instagram" className="h-6 w-6" />;
+       case 'tiktok':
+        return <img src={tiktokIcon} alt="Tiktok" className="h-6 w-6" />;
       case 'website':
         return <img src={websiteIcon} alt="Website" className="h-6 w-6" />;
       default:
